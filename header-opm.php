cdc606e9373a006b747a3282d42d521c876fb3bc
Hasin Hayder <hasin@leevio.com>
2014-12-20 04:21:13 +0600

    Title Added

--- header-opm.php
+++ header-opm.php
@@ -11,10 +11,11 @@ global $opm;
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php wp_title('|', true, 'right'); ?></title>
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
     <?php wp_head(); ?>
 </head>
@@ -24,11 +25,12 @@ global $opm;
     <div class="container">
         <div class="row">
             <div class="col-md-4">
-                <h1 class="logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <h1 class="logo"><a href="<?php echo esc_url(home_url('/')); ?>"
+                                    rel="home"><?php bloginfo('name'); ?></a></h1>
             </div>
             <div class="col-md-8">
                 <div id="navigation">
-                    <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class'=>'pull-right list-inline' ) ); ?>
+                    <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'pull-right list-inline')); ?>
                 </div>
             </div>
         </div>
@@ -40,9 +42,11 @@ global $opm;
         <div class="row">
             <div class="col-md-12">
                 <div class="bannerhead">
-                    <h1><?php bloginfo( 'description' ); ?></h1>
-                    <a href="<?php echo $opm['opm_banner_button1_url'];?>" class="bh-button"><?php echo $opm['opm_banner_button1_text'];?></a>
-                    <a href="<?php echo $opm['opm_banner_button2_url'];?>" class="bh-button"><?php echo $opm['opm_banner_button2_text'];?></a>
+                    <h1><?php bloginfo('description'); ?></h1>
+                    <a href="<?php echo $opm['opm_banner_button1_url']; ?>"
+                       class="bh-button"><?php echo $opm['opm_banner_button1_text']; ?></a>
+                    <a href="<?php echo $opm['opm_banner_button2_url']; ?>"
+                       class="bh-button"><?php echo $opm['opm_banner_button2_text']; ?></a>
                 </div>
             </div>
         </div>
